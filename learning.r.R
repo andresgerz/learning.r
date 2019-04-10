9483aca8db8ab2369525d8607d0b695ea76e68f2
andresger <andres.gerz@gmail.com>
2019-04-10 14:27:54 -0300

    My first commit

---
+++ learning.r.R
@@ -0,0 +1,31 @@
+# age = c(1,3,5,7,9,11,13,15)
+# height = c(76,86,95,109,122,134,144,156)
+# 
+# datas1 = data.frame(age, height)
+# 
+# 
+# plot(data1)
+# 
+# regression_age_height = lm(height~age, data = datas1)
+# 
+# summary(regression_age_height)
+# abline(regression_age_height, col = "red")
+
+##################################################################################
+# 
+# df_pearson2 = read.table("http://aprender.uib.es/Rdir/pearson.txt", header = TRUE)
+# View(df_pearson2)
+# 
+# plot(df_pearson2)
+# 
+# regression_sons_fathers = lm(Hijos~Padres, data = df_pearson2)
+# abline(regression_sons_fathers, col = "blue" )
+# 
+# summary(regression_sons_fathers)
+
+##################################################################################
+
+
+
+
+
